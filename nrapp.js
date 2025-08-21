6025b320ea4492a7eb8646c5ad9d244f5e3c04a5
rajasekhar-p3 <rajasekharp638@gmail.com>
2025-08-21 13:38:11 +0530

    Update nrapp.js

--- nrapp.js
+++ nrapp.js
@@ -1,9 +1,1 @@
-// Smooth scrolling for nav links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
+
